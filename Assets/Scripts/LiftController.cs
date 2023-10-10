26286abd59c89ebd765ca6ce1710fd350feb2775
ESCG\537387 <537387@escg.ac.uk>
2023-10-10 17:01:12 +0100

    no message

--- Assets/Scripts/LiftController.cs
+++ Assets/Scripts/LiftController.cs
@@ -1,16 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LiftController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
+    public int bottomheight;
+    public int topheight;
+    public GameObject lift;
+    public float speed;
+    private bool liftup = false;
+    private void Update()
     {
-        Event.current.onLiftTriggerEnter += OnLiftOpen;
+
+        if (liftup)
+        {
+            if (lift.transform.position.y <= topheight)
+            {
+                lift.transform.position += transform.up * speed * Time.deltaTime;
+            }
+        }
+        else if (liftup == false)
+        {
+            if (lift.transform.position.y >= bottomheight)
+            {
+                lift.transform.position += -transform.up * speed * Time.deltaTime;
+            }
+        }
+        liftup = false;
     }
-    private void OnLiftOpen()
+    private void OnTriggerStay(Collider other)
     {
-        LeanTween.moveLocalY(gameObject, 1.6f, 1f).setEaseOutQuad();
+        if (other.tag == "Player1")
+        {
+            Debug.Log("up");
+            liftup = true;
+        }
     }
 }
